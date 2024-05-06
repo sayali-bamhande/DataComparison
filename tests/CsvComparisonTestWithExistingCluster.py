31f4e2614e8dfee4764e6d3777d7f1a1349a89b4
Sayali Subhash Bamhande <Sayali.Bamhande@coforge.com>
2024-05-06 16:28:52 +0530

    commented upload to GCS file

--- tests/CsvComparisonTestWithExistingCluster.py
+++ tests/CsvComparisonTestWithExistingCluster.py
@@ -5,10 +5,10 @@ from pythonProject.utils.UploadFileToGCS import upload_to_gcs
 from pythonProject.utils.ConstantsData import region, project_id, cluster_name, bucket_name
 
 
-local_file_path = "C:\\Users\\Sayali.Bamhande\\PycharmProjects\\pythonProject\\pythonProject\\utils\\CompareCSVAndGenerateOutput.py"
+#local_file_path = "C:\\Users\\Sayali.Bamhande\\PycharmProjects\\pythonProject\\pythonProject\\utils\\CompareCSVAndGenerateOutput.py"
 # destination_blob_name = f'CsvDataComparisonUsingDataframe.py'
 destination_blob_name = f'CompareCSVAndGenerateOutput.py'
-upload_to_gcs(local_file_path, bucket_name, destination_blob_name)
+#upload_to_gcs(local_file_path, bucket_name, destination_blob_name)
 startTheCluster(project_id,region,cluster_name)
 zone = 'us-central1-f'
 SubmitJobToCluster(project_id,region,cluster_name,'CompareCSVAndGenerateOutput.py')

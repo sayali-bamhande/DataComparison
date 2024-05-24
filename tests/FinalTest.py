bad9449497195ed5814c32103b60c338e2b97a44
Sayali Subhash Bamhande <Sayali.Bamhande@coforge.com>
2024-05-24 21:10:21 +0530

    fixing error of jenkins job

--- tests/FinalTest.py
+++ tests/FinalTest.py
@@ -1,24 +1,18 @@
 import argparse
 import os
-import sys
 
-from utils.SubmitJobToCluster import SubmitJobToCluster
+from utils.CreateClusterTest import create_cluster, delete_cluster
+from utils.UploadFileToGCS import UploadFileToGCS
 from utils.ConstantsData import region, project_id, bucket_name, new_cluster_name
+from utils.SubmitJobToCluster import SubmitJobToCluster
+
 
 fileName = 'CompareCSVAndGenerateOutput.py'
 zone = 'us-central1-f'
 parser = argparse.ArgumentParser(description="Process some arguments.")
 parser.add_argument('--params', nargs='*', help='Argument should be in Key-value pairs', default=[])
 
-# args = parser.parse_args()
-arg = []
-for i in range(2, len(sys.argv)):
-    print(sys.argv[i], end=" ")
-    arg.append(sys.argv[i])
-
-# if 'params' in arg[0]:
-#     print("first parameter is params")
-#     arg.pop(0)
+args = parser.parse_args()
 
 # Parse the key-value pairs and store them in the config module
 # arg = []
@@ -27,7 +21,7 @@ for i in range(2, len(sys.argv)):
 #     TestData.params[key] = value
 #
 # TestData.iteration = TestData.params['count']
-print(f"Arguments stored in config: {arg}")
+# print(f"Arguments stored in config: {TestData.params}")
 
 current_file = os.path.abspath(__file__)
 root_dir = os.path.dirname(current_file)
@@ -36,7 +30,7 @@ root_dir = os.path.dirname(root_dir)
 file_path = os.path.join(root_dir, "utils/"f"{fileName}")
 destination_blob_name = f'{fileName}'
 
-# UploadFileToGCS(file_path, bucket_name, destination_blob_name)
+UploadFileToGCS(file_path, bucket_name, destination_blob_name)
 # create_cluster(project_id, region, new_cluster_name, zone)
-SubmitJobToCluster(project_id, region, new_cluster_name, f"{arg}")
+SubmitJobToCluster(project_id, region, new_cluster_name, f'{fileName}', args.params)
 # delete_cluster(project_id, region, new_cluster_name)
--- utils/CompareCSVAndGenerateOutput.py
+++ utils/CompareCSVAndGenerateOutput.py
@@ -6,6 +6,7 @@ import pandas as pd
 import sys
 
 
+
 def compare_and_generate_output(csv1_path, csv2_path, bucket_name, output_format, iteration):
     # Read CSV files
     if csv1_path.lower().endswith('.csv'):
@@ -23,7 +24,7 @@ def compare_and_generate_output(csv1_path, csv2_path, bucket_name, output_format
 
     # Initialize an empty list to store rows
     rows = []
-    report = [f"This process is for iteration : {iteration} ", f"No of rows in Source file(DB2) : {len(df1)}",
+    report = [f"This process is for {iteration} iteration", f"No of rows in Source file(DB2) : {len(df1)}",
               f"No of rows in Target file(BigQuery) : {len(df2)}"]
     # Iterate through each row and compare values
     count = 0
@@ -110,9 +111,7 @@ def compare_and_generate_output(csv1_path, csv2_path, bucket_name, output_format
 # print(f"argument name = {TestData.params['count']}")
 # print(f"file variable = {TestData.iteration}")
 def main(my_list):
-   # print("###########", my_list)
-    total_comparison = my_list[0]
-    # my_list.pop(0)
+    print("###########", my_list)
     iteration = 1
     for i in range(1, len(my_list) - 1):
         if i % 2 != 0:
@@ -122,10 +121,10 @@ def main(my_list):
             compare_and_generate_output(source_file, target_file, 'mybucket_hsbc', "csv", iteration)
             print("=============================================================================================")
             iteration = iteration + 1
-    # file1 = 'gs://mybucket_hsbc/db2_changed.csv'
-    # file2 = 'gs://mybucket_hsbc/BigQueryData.csv'
-    # file1 = 'C:\\Users\\Sayali.Bamhande\\Downloads\\xl\\db2_changed.csv'
-    # file2 = 'C:\\Users\\Sayali.Bamhande\\Downloads\\xl\\BigQueryData.csv'
+    file1 = 'gs://mybucket_hsbc/db2_changed.csv'
+    file2 = 'gs://mybucket_hsbc/BigQueryData.csv'
+    file1 = 'C:\\Users\\Sayali.Bamhande\\Downloads\\xl\\db2_changed.csv'
+    file2 = 'C:\\Users\\Sayali.Bamhande\\Downloads\\xl\\BigQueryData.csv'
 
 
 # compare_and_generate_output(file1, file2, 'mybucket_hsbc', "csv", 1)
@@ -140,22 +139,22 @@ def parse_arguments():
 # Example usage
 if __name__ == '__main__':
     print("script WILL start with new change....")
-    n = len(sys.argv)
-    print(f"Total number of arguments are : {n - 1}")
+    n=len(sys.argv)
+    print(f"total number of arguments are : {n-1}")
     print(f"User wants to perform  {sys.argv[1]} comparison")
 
     print("script is running....")
 
     for i in range(1, n):
         print(sys.argv[i], end=" ")
-    # list.append(sys.argv[i])
+       # list.append(sys.argv[i])
     list = []
     for i in range(1, n):
         key, value = sys.argv[i].split('=')
         list.append(value)
 
     print(f"List values are ## {list}")
-    # list.append(sys.argv[i])
+    list.append(sys.argv[i])
     # runtime_args = {}
     # if args.params:
     #     print("script started")
@@ -164,6 +163,6 @@ if __name__ == '__main__':
     #         key, value = arg.split('=')
     #         runtime_args[key] = value
     #         list.append(value)
-    # list.pop(0)
+    #list.pop(0)
     print(f"calling main function now list : {list}")
     main(list)

61dc09dd5d902678efc66485618dc2f4182f86b3
Sayali Subhash Bamhande <Sayali.Bamhande@coforge.com>
2024-05-24 16:05:22 +0530

    added code for runtime arguments

--- tests/FinalTest.py
+++ tests/FinalTest.py
@@ -6,17 +6,16 @@ from utils.CreateClusterTest import create_cluster, delete_cluster
 from utils.SubmitJobToCluster import SubmitJobToCluster
 from utils.ConstantsData import region, project_id, bucket_name, new_cluster_name
 
-
 fileName = 'CompareCSVAndGenerateOutput.py'
 zone = 'us-central1-f'
 parser = argparse.ArgumentParser(description="Process some arguments.")
 parser.add_argument('--params', nargs='*', help='Argument should be in Key-value pairs', default=[])
 
-#args = parser.parse_args()
-list=[]
+# args = parser.parse_args()
+arg = []
 for i in range(1, len(sys.argv)):
     print(sys.argv[i], end=" ")
-    list.append(sys.argv[i])
+    arg.append(sys.argv[i])
 
 # Parse the key-value pairs and store them in the config module
 # arg = []
@@ -34,7 +33,7 @@ root_dir = os.path.dirname(root_dir)
 file_path = os.path.join(root_dir, "utils/"f"{fileName}")
 destination_blob_name = f'{fileName}'
 
-#UploadFileToGCS(file_path, bucket_name, destination_blob_name)
-create_cluster(project_id, region, new_cluster_name, zone)
-SubmitJobToCluster(project_id, region, new_cluster_name, f'{fileName}',list)
+# UploadFileToGCS(file_path, bucket_name, destination_blob_name)
+#create_cluster(project_id, region, new_cluster_name, zone)
+SubmitJobToCluster(project_id, region, new_cluster_name, f'{fileName}', arg)
 # delete_cluster(project_id, region, new_cluster_name)

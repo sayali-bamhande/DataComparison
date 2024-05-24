1abf65c5e14d5e8696edb122f48dc064c13545b3
Sayali Subhash Bamhande <Sayali.Bamhande@coforge.com>
2024-05-24 15:43:42 +0530

    added code for runtime arguments

--- tests/FinalTest.py
+++ tests/FinalTest.py
@@ -1,5 +1,7 @@
 import argparse
 import os
+import sys
+
 from utils.CreateClusterTest import create_cluster, delete_cluster
 from utils.SubmitJobToCluster import SubmitJobToCluster
 from utils.ConstantsData import region, project_id, bucket_name, new_cluster_name
@@ -10,7 +12,11 @@ zone = 'us-central1-f'
 parser = argparse.ArgumentParser(description="Process some arguments.")
 parser.add_argument('--params', nargs='*', help='Argument should be in Key-value pairs', default=[])
 
-args = parser.parse_args()
+#args = parser.parse_args()
+list=[]
+for i in range(1, len(sys.argv)):
+    print(sys.argv[i], end=" ")
+    list.append(sys.argv[i])
 
 # Parse the key-value pairs and store them in the config module
 # arg = []
@@ -19,7 +25,7 @@ args = parser.parse_args()
 #     TestData.params[key] = value
 #
 # TestData.iteration = TestData.params['count']
-print(f"Arguments stored in config: {args.params}")
+print(f"Arguments stored in config: {list}")
 
 current_file = os.path.abspath(__file__)
 root_dir = os.path.dirname(current_file)
@@ -29,6 +35,6 @@ file_path = os.path.join(root_dir, "utils/"f"{fileName}")
 destination_blob_name = f'{fileName}'
 
 #UploadFileToGCS(file_path, bucket_name, destination_blob_name)
-#create_cluster(project_id, region, new_cluster_name, zone)
-SubmitJobToCluster(project_id, region, new_cluster_name, f'{fileName}', args.params)
+create_cluster(project_id, region, new_cluster_name, zone)
+SubmitJobToCluster(project_id, region, new_cluster_name, f'{fileName}',list)
 # delete_cluster(project_id, region, new_cluster_name)
